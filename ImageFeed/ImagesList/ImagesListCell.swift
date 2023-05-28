a6296b29624b095f2c080412d4957a5a7255ee7b
CodeByViktor <cool-kyl@yandex.ru>
2023-05-29 01:36:20 +0300

    Added mockImages, tableView and setting up Launch Screen

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Виктор on 29.05.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Виктор on 28.05.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
