49215f5dcd3900a568bf48b27a499907ff1cc955
CodeByViktor <cool-kyl@yandex.ru>
2023-06-10 00:32:17 +0300

    Create profile screen and controller

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Виктор on 09.06.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var photoImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var linkLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    override func viewDidLoad() {
+        
+    }
+    @IBAction func logout(_ sender: Any) {
+    }
+}
