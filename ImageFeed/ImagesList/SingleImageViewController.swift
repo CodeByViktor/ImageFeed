df975746fc47e279bc03f15a3b4a2cc60f4f10e8
CodeByViktor <cool-kyl@yandex.ru>
2023-06-21 20:42:43 +0300

    Change Profile page from IB to code

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
-    var image: UIImage!
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
     
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
         imageView.image = image
+        
+        rescaleAndCenterImageInScrollView(image: image)
+        
+    }
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let sharingController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        //sharingController.popoverPresentationController?.sourceView = self.view
+        self.present(sharingController, animated: true)
+    }
+    @IBAction func didTabBackButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
+
+extension SingleImageViewController {
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,14 +8,62 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    
-    @IBOutlet private weak var photoImageView: UIImageView!
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var linkLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
     override func viewDidLoad() {
+        let avatarImage = UIImage(named: "Photo")
+        let avatarView = UIImageView()
+        avatarView.image = avatarImage
+        avatarView.translatesAutoresizingMaskIntoConstraints = false
         
+        let exitButtonImage = UIImage(systemName: "ipad.and.arrow.forward")
+        let exitButton = UIButton.systemButton(with: exitButtonImage!,
+                                               target: self,
+                                               action: #selector(logout))
+        exitButton.tintColor = UIColor(named: "YP Red")
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        nameLabel.textColor = UIColor(named: "YP White")
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let linkLabel = UILabel()
+        linkLabel.text = "@ekaterina_nov"
+        linkLabel.font = UIFont.systemFont(ofSize: 13)
+        linkLabel.textColor = UIColor(named: "YP Grey")
+        linkLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let descLabel = UILabel()
+        descLabel.text = "Hello, world!"
+        descLabel.font = UIFont.systemFont(ofSize: 13)
+        descLabel.textColor = UIColor(named: "YP White")
+        descLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(avatarView)
+        view.addSubview(exitButton)
+        view.addSubview(nameLabel)
+        view.addSubview(linkLabel)
+        view.addSubview(descLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarView.widthAnchor.constraint(equalToConstant: 70),
+            avatarView.heightAnchor.constraint(equalToConstant: 70),
+            avatarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            
+            nameLabel.topAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor),
+            linkLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            linkLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor),
+            descLabel.topAnchor.constraint(equalTo: linkLabel.bottomAnchor, constant: 8),
+            descLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor),
+            
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            exitButton.centerYAnchor.constraint(equalTo: avatarView.centerYAnchor)
+        ])
     }
-    @IBAction func logout(_ sender: Any) {
+    
+    @objc
+    private func logout(_ sender: Any) {
     }
 }
