29f81dca0565465fc337e210e9e3ea8ed5eb392c
CodeByViktor <cool-kyl@yandex.ru>
2023-07-03 14:22:19 +0300

    Added Auth screens

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Виктор on 03.07.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Виктор on 03.07.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        
+    }
+}
