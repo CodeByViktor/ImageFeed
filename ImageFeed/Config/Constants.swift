fc03bed8a97de4873ed15974e747a1d694af38a5
CodeByViktor <cool-kyl@yandex.ru>
2023-07-03 13:02:53 +0300

    Added constants

---
+++ ImageFeed/Config/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Виктор on 30.06.2023.
+//
+
+import Foundation
+
+let defaultBaseURL = URL(string: "https://api.unsplash.com")
+let accessKey = "HdOZmJg-AxkwrDC8rIePQPEREv2TE_bHvzhLeutLaCM"
+let secretKey = "aAYiGXVXzJ3ewkQyevhxBe9oiK5YgKaTqe_ec_aQwT4"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
